6b16fe97bb8601b060838fda27b090b2f4809c27
scottyvg <svangilder@gmail.com>
2020-12-06 21:07:19 -0700

    add day 2 2020 part2 solution

--- 2020/day/2/part2.py
+++ 2020/day/2/part2.py
@@ -1,8 +1,5 @@
 import re
 
-'''
-TODO: Complete problem
-'''
 def main():
     with open("input.txt", "r") as input_file:
         input_data = input_file.read()
@@ -11,16 +8,26 @@ def main():
 
     count = 0
 
+    def Convert(string): 
+        list1=[] 
+        list1[:0]=string 
+        return list1 
+
     for i in range(len(li)):
+        plist = ""
         password_regex = re.compile(r'(\d+)-(\d+)\s([a-z]):\s(.*)')
         regex_match = password_regex.search(li[i])
         a = int(regex_match.group(1))
         b = int(regex_match.group(2))
-        c = int(regex_match.group(4).count(regex_match.group(3)))
-        
-        if c >= a and c <= b:
+        c = regex_match.group(3)
+        d = regex_match.group(4)
+        plist = Convert(d)
+        if plist[a-1] == c and plist[b-1] == c:
+            pass
+        elif plist[a-1] == c:
             count += 1
-
+        elif plist[b-1] == c:
+            count += 1
     print (count)
 
 if __name__ == "__main__":
