5721663c5d70396c91ab55d545f9b0f92ce32165
scottyvg <svangilder@gmail.com>
2020-12-06 20:20:03 -0700

    add day 2 2020 - with part1 solution

---
+++ 2020/day/2/part2.py
@@ -0,0 +1,28 @@
+import re
+
+'''
+TODO: Complete problem
+'''
+def main():
+    with open("input.txt", "r") as input_file:
+        input_data = input_file.read()
+
+    li = list(input_data.split("\n"))
+
+    count = 0
+
+    for i in range(len(li)):
+        password_regex = re.compile(r'(\d+)-(\d+)\s([a-z]):\s(.*)')
+        regex_match = password_regex.search(li[i])
+        a = int(regex_match.group(1))
+        b = int(regex_match.group(2))
+        c = int(regex_match.group(4).count(regex_match.group(3)))
+        
+        if c >= a and c <= b:
+            count += 1
+
+    print (count)
+
+if __name__ == "__main__":
+    main()
+
