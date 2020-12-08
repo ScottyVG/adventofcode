18eddab129a5fe80060f9ecf6d30af131be3fcb5
scottyvg <svangilder@gmail.com>
2020-12-08 08:48:38 -0700

    add day 3 2020 part1 solution

--- 2020/day/3/part1.py
+++ 2020/day/3/part1.py
@@ -11,38 +11,24 @@ def main():
         list1[:0]=string 
         return list1 
 
-    turns = 0
+    turns = -3
+    count = 0
 
     for i in range(len(li)):
-        # slist = Convert(li[i])
-        # print(slist)
-        print(turns)
+        
+        slist = Convert(li[i])
+        turns += 3
+
         if turns >= 31:
-            turns = 0
+            turns = turns - 31
+
+        if i == 0:
+            pass
         else:
-            turns += 3
-        
-        # if i == 0:
-        #     pass
-        # else:
-        #     print(slist[turns+1])
-            # print("")
-            # print("###################################")
-            # print(li[i])
-            # print(li[i].index(turns))
-            # print(li[i])
-
-
-
-
-    #     password_regex = re.compile(r'(\d+)-(\d+)\s([a-z]):\s(.*)')
-    #     regex_match = password_regex.search(li[i])
-    #     a = int(regex_match.group(1))
-    #     b = int(regex_match.group(2))
-    #     c = int(regex_match.group(4).count(regex_match.group(3)))
-        
-    #     if c >= a and c <= b:
-    #         count += 1
+            if slist[turns] == '#':
+                count += 1
+    
+    print (count)
 
 if __name__ == "__main__":
     main()
